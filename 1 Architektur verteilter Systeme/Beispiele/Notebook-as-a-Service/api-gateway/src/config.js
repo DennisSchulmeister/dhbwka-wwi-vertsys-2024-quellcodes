8546a0ae39fd77886eae81d05972a81102e9b223
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-02-20 00:51:16 +0100

    Notebook-as-a-Service: API-Gateway

--- 1 Architektur verteilter Systeme/Beispiele/Notebook-as-a-Service/api-gateway/src/config.js
+++ 1 Architektur verteilter Systeme/Beispiele/Notebook-as-a-Service/api-gateway/src/config.js
@@ -2,6 +2,54 @@ import {logger}     from "@dschulmeis/naas-common/src/utils.js";
 import YAML         from "yaml";
 import fs           from "node:fs/promises";
 
+// Erlaubte Schlüsselwörter für die Syntaxprüfung:
+// Der Typ "list" ist eine Liste von Strings oder ein einzelner String.
+// Alle anderen Typen entsprechen ihrem JavaScript-Gegenstück.
+const IF_SYNTAX = {
+    type: "object",
+    properties: {
+        "url":     {type: "list"},
+        "method":  {type: "list"},
+        "headers": {
+            type: "object",
+            properties: {
+                "*": {type: "list"},
+            },
+        },
+    }
+};
+
+const THEN_ELSE_SYNTAX = {
+    type: "object",
+    properties: {
+        "status-code": {type: "number"},
+        "headers":     {type: "object"},
+        "body":        {type: "any"},
+        "forward-to":  {type: "list"},
+        "finish":      {type: "boolean"},
+    }
+};
+
+export const CONFIG_SYNTAX = {
+    type: "object",
+    properties: {
+        "variables": {
+            type: "object"
+        },
+        "rules": {
+            type: "array",
+            items: {
+                type: "object",
+                properties: {
+                    "if":   IF_SYNTAX, 
+                    "then": THEN_ELSE_SYNTAX,
+                    "else": THEN_ELSE_SYNTAX,
+                },
+            },
+        },
+    },
+};
+
 // Gateway-Konfiguration als Singleton
 export let config = {};
 
@@ -32,13 +80,16 @@ export async function readConfigFile(configFileName) {
     
         let configFile = await fs.readFile(configFileName, "utf-8");
         config = YAML.parse(configFile);
-    
+
         // ${config.}-Verweise in der Konfiguration auflösen
         let redo = false;
 
         do {
             [config, redo] = resolveConfigReferences(config, "config", config);
         } while (redo);
+
+        // Einfache Syntaxprüfung, um Schreibfehler gleich bei Programmstart abzufangen
+        config = syntaxCheck(config, CONFIG_SYNTAX);
 
         return config;
     } catch (error) {
@@ -63,23 +114,30 @@ export async function readConfigFile(configFileName) {
  * @param {Object} source Quellobjekt, auf das sich die Referenzen verweisen
  * @param {string} name Name des Quellobjekts in der Konfiguration, z.B. "config" oder "request"
  * @param {Any} config Zu bearbeitende Konfiguration (Achtung: Kopie verwenden!)
+ * @param {Any} defaultValue Standardwert, wenn ein Wert nicht ermittelt werden kann
  * @returns {Array} Neues `config`_-Objekt und `redo` Flag
  */
-export function resolveConfigReferences(source, name, config) {
+export function resolveConfigReferences(source, name, config, defaultValue) {
     let redo = false, redo1 = false;
 
     if (typeof config === "string" || config instanceof String) {
         config = config.trim();
+        let prefix = `\$\{${name}.`;
 
-        if (config.startsWith(`\$\{${name}.`) && config.endsWith("}")) {
-            let path   = config.slice(2, config.length - 1);
+        if (config.startsWith(prefix) && config.endsWith("}")) {
+            let path   = config.slice(prefix.length, config.length - 1);
             let target = source;
 
-            for (let pathItem of path.split(".").slice(1)) {
+            for (let pathItem of path.split(".")) {
                 target = target[pathItem];
 
                 if (!target || target === source) {
-                    throw new Error(`Fehler in der Konfigurationsdatei: Verweis '${config}' kann nicht aufgelöst werden.`);
+                    if (defaultValue === undefined) {
+                        throw new Error(`Fehler in der Konfigurationsdatei: Verweis '${config}' kann nicht aufgelöst werden.`);
+                    } else {
+                        target = defaultValue;
+                        break;
+                    }
                 }
             }
 
@@ -87,15 +145,110 @@ export function resolveConfigReferences(source, name, config) {
         }
     } else if (Array.isArray(config)) {
         for (let i in config) {
-            [config[i], redo1] = resolveConfigReferences(source, name, config[i]);
+            [config[i], redo1] = resolveConfigReferences(source, name, config[i], defaultValue);
             redo = redo || redo1;
         }
     } else if (typeof config === "object") {
         for (let key of Object.keys(config)) {
-            [config[key], redo1] = resolveConfigReferences(source, name, config[key]);
+            [config[key], redo1] = resolveConfigReferences(source, name, config[key], defaultValue);
             redo = redo || redo1;
         }
     }
 
     return [config, redo];
+}
+
+/**
+ * Konfiguration auf grobe Rechtschreibfehler prüfen. Wirft eine Exception,
+ * falls welche gefunden werden. Durchläuft die Konfiguration rekursiv und
+ * ersetzt dabei ggf. Inhalte durch ihre geeigneten Datentypen.
+ * 
+ * @param {any} config Zu prüfende Konfiguration
+ * @param {Object} syntaxDefinition Erlaubte Schlüsselwörter (Konstanten oben)
+ * @returns {any} Neuer Wert für die geprüfte Konfiguration.
+ */
+export function syntaxCheck(config, syntaxDefinition) {
+    switch (syntaxDefinition.type) {
+        case "any": {
+            return config;
+        }
+
+        case "object": {
+            if (Array.isArray(config) || typeof config !== "object" || config instanceof String) {
+                throw new Error("Fehler in der Konfigurationsdatei: Das folgende Konstrukt ist kein YAML-Objekt:\n" + YAML.stringify(config));
+            }
+
+            if (syntaxDefinition.properties) {
+                for (let propertyName of Object.keys(config)) {
+                    let propertySyntax = syntaxDefinition.properties[propertyName] || syntaxDefinition.properties["*"];
+
+                    if (propertySyntax === undefined) {
+                        throw new Error(`Fehler in der Konfigurationsdatei: Unbekanntes Schlüsselwort '${propertyName}' in:\n` + YAML.stringify(config));
+                    }
+
+                    config[propertyName] = syntaxCheck(config[propertyName], propertySyntax);
+                }
+            }
+
+            return config;
+        }
+
+        case "array": {
+            if (!Array.isArray(config)) {
+                throw new Error("Fehler in der Konfigurationsdatei: Das folgende Konstrukt ist kein YAML-Array:\n" + YAML.stringify(config));
+            }
+
+            if (syntaxDefinition.items) {
+                for (let index in config) {
+                    config[index] = syntaxCheck(config[index], syntaxDefinition.items);
+                }
+            }
+
+            return config;
+        }
+
+        case "list": {
+            if (!Array.isArray(config)) config = [config];
+
+            if (config.length === 0) {
+                throw new Error("Fehler in break;der Konfigurationsdatei: Liste darf nicht leer sein.");
+            }
+
+            for (let index in config) {
+                if (typeof config[index] === "object") {
+                    throw new Error("Fehler in der Konfigurationsdatei: Das folgende Konstrukt lässt sich nicht als String interpretieren:\n" + YAML.stringify(config[index]));
+                } else {
+                    config[index] = `${config[index]}`.trim();
+                }
+            }
+
+            return config;
+        }
+
+        case "number": {
+            if (typeof config === "number") {
+                return config;
+            } else if (!Number.isNaN(parseFloat(config))) {
+                return parseFloat(config);
+            } else {
+                throw new Error("Fehler in der Konfigurationsdatei: Das folgende Konstrukt lässt sich nicht als Nummer interpretieren:\n" + YAML.stringify(config));
+            }
+        }
+
+        case "boolean": {
+            if (typeof config === "boolean") {
+                return config;
+            } else if (["true", "yes", "y", "ja", "j", "x"].includes(`${config}`.trim().toLowerCase())) {
+                return true;
+            } else if (["false", "no", "nein", "n", "-"].includes(`${config}`.trim().toLowerCase())) {
+                return false;
+            } else {
+                throw new Error("Fehler in der Konfigurationsdatei: Das folgende Konstrukt lässt sich nicht als Boolean interpretieren:\n" + YAML.stringify(config));
+            }
+        }
+
+        default: {
+            throw new Error(`Programmierfehler in der Syntaxprüfung: Typprüfung für Typ '${syntaxDefinition.type}' ist nicht definiert!`);
+        }
+    }
 }
--- 1 Architektur verteilter Systeme/Beispiele/Notebook-as-a-Service/api-gateway/src/controllers/configured-handlers.controller.js
+++ 1 Architektur verteilter Systeme/Beispiele/Notebook-as-a-Service/api-gateway/src/controllers/configured-handlers.controller.js
@@ -1,3 +1,14 @@
+import {copyConfig}              from "../config.js";
+import {resolveConfigReferences} from "../config.js";
+
+import {wrapAsync}               from "@dschulmeis/naas-common/src/utils.js";
+import {logger}                  from "@dschulmeis/naas-common/src/utils.js";
+import YAML                      from "yaml";
+import http                      from "node:http";
+
+// Zuletzt verwendeter Index beim Load Balancing
+let loadBalancer = {};
+
 /**
  * Diese Funktion registriert die unten ausprogrammierten Route Handler der
  * Express Application hinzu.
@@ -5,4 +16,268 @@
  * @param {Express.Application} app Express Application
  */
 export default function registerRoutes(app) {
+    app.all("/*", wrapAsync(handleRequest));
 };
+
+/**
+ * Generische HTTP-Handler-Funktion zur Ausführung der konfigurierten Regeln
+ * des API-Gateways.
+ * 
+ * @param {Express.Request} req HTTP-Anfrage
+ * @param {Express.Response} res HTTP-Antwort
+ * @param {Function} next `next()`-Funktion von Express
+ */
+async function handleRequest(req, res, next) {
+    // Konfiguration kopieren und ${request.}-Referenzen auflösen
+    let config = copyConfig();
+    let redo   = false;
+
+    do {
+        [config, redo] = resolveConfigReferences(req.headers, "request.headers", config, "");
+    } while (redo);
+
+    do {
+        [config, redo] = resolveConfigReferences(req, "request", config);
+    } while (redo);
+
+    logger.debug("Konfiguration nach Auflösen der Request-Parameter: \n", YAML.stringify(config));
+    logger.debug("Load Balancer Status vor Bearbeitung des Requests:\n", YAML.stringify(loadBalancer));
+
+    // Zutreffende Regeln ausführen
+    for (let rule of config.rules || []) {
+        let actions = matchRule(rule, req);
+        let finish  = await executeActions(actions, req, res);
+
+        if (finish) break;
+    }
+
+    next();
+}
+
+/**
+ * Diese Funktion wertet eine einzelne Konfigurationsregel aus und gibt die aufgrund
+ * der Regel auszuführenden Aktionen zurück. Die Konfigurierte Regel muss hierfür
+ * folgenden Aufbau besitzen:
+ * 
+ * ```yml
+ * if:
+ *   prüfung1: ...
+ *   prüfung2: ...
+ * then:
+ *   aktion1: ...
+ *   aktion2: ...
+ * else:
+ *   aktion1: ...
+ *   aktion2: ...
+ * ```
+ * 
+ * Alle drei Bestandteile sind optional. Wenn alle `if`-Prüfungen zutreffen, wird das `then`-Objekt
+ * zurückgeliefert, sonst das `else`-Objekt. Fehlt das zurückzugebende Objekt, wird ein leeres
+ * Objekt zurückgegeben.
+ * 
+ * Folgende Prüfungen sind derzeit im `if`-Zweig erlaubt:
+ * 
+ *   * `url` (String oder Liste von Strings): <br>
+ *      Prüfung der URL. Wenn diese auf "*" endet, wird nur der Prefix gematcht. Sonst volle Gleichheit.
+ * 
+ *   * `method` (String oder Liste von Strings): <br>
+ *      Prüfung des HTTP Verbs. Groß-/Kleinschreibung wird ignoriert.
+ * 
+ *   * `headers` (Objekt mit HTTP-Headern, Werte sind String oder Liste von Strings): <br>
+ *      Prüfung eines oder mehrerer HTTP-Header. Alle Header müssen vorhanden sein und den angegebenen
+ *      Wert besitzen. "*" kann für Wildcard- / Prefix-Matching genutzt werden.
+ * 
+ * Die zurückgegebenen Aktionen werden hier nicht validiert, sondern müssen hierfür einfach
+ * an die nächste Funktion `executeActions()` übergeben werden.
+ *
+ * @param {Object} rule Zu prüfende Regel
+ * @param {Express.Request} req HTTP-Anfrage
+ * @returns {Object} Auszuführende Aktionen oder leeres Objekt
+ */
+function matchRule(rule, req) {
+    let matched   = false;
+    let urlPrefix = "";
+    let result    = {};
+
+    function _matchStringPattern(value, pattern) {
+        if (pattern.endsWith("*")) {
+            pattern = pattern.slice(0, pattern.length - 1);
+            if (`${value}`.startsWith(pattern)) return pattern;
+        } else {
+            if (`${value}` === `${pattern}`) return pattern;
+        }
+    }
+
+    if ("if" in rule) {
+        matched = false;
+
+        for (let keyword of Object.keys(rule.if)) {
+            switch (keyword) {
+                case "url": {
+                    for (let urlPattern of rule.if.url) {
+                        urlPrefix = _matchStringPattern(req.originalUrl, urlPattern);
+
+                        matched = !!urlPrefix;
+                        if (matched) break;
+                    }
+
+                    break;
+                }
+
+                case "method": {
+                    for (let methodPattern of rule.if.method) {
+                        methodPattern = methodPattern.toUpperCase();
+                        matched = !!_matchStringPattern(req.method.toUpperCase(), methodPattern);
+                        if (matched) break;
+                    }
+
+                    break;
+                }
+
+                case "headers": {
+                    for (let headerName of Object.keys(rule.if.headers)) {
+                        for (let headerValuePattern of rule.if.headers[headerName]) {
+                            matched = !!_matchStringPattern(req.headers[headerName], headerValuePattern);
+                            if (matched) break;
+                        }
+
+                        if (!matched) break;
+                    }
+
+                    break;
+                }
+            }
+
+            if (!matched) break;
+        }
+    } else {
+        matched = true;
+    }
+
+    if (matched) result = rule.then || {};
+    else result = rule.else || {};
+
+    result._urlPrefix = urlPrefix;
+    return result;
+}
+
+/**
+ * Diese Funktion führt die zuvor mit `matchRule()` ermittelten Aktionen aus. Folge Aktionen
+ * sind dabei derzeit erlaubt:
+ * 
+ *   * `status-code` (numerisch):
+ *      HTTP-Statuscode setzen
+ * 
+ *   * `headers` (Objekt):
+ *      HTTP-Header setzen
+ * 
+ *   * `body` (Irgendetwas):
+ *      HTTP-Body setzen (wird ggf. nach JSON umgewandelt)
+ * 
+ *   * `forward-to` (String oder Liste mit Strings):
+ *      Weiterleitung an Backendsystem, bei Liste mit Load Balancing
+ * 
+ *   * `finish` (Boolean):
+ *      Regelverarbeitung nach dieser Regel abbrechen
+ * 
+ * @param {Object} actions Auszuführende Aktion
+ * @param {Express.Request} req HTTP-Anfrage
+ * @param {Express.Response} res HTTP-Antwort
+ * @returns {Boolean} Flag für Finish-Regel
+ */
+async function executeActions(actions, req, res) {
+    let finish = false;
+
+    for (let keyword of Object.keys(actions || {})) {
+        switch (keyword) {
+            case "status-code": {
+                res.status(actions["status-code"]);
+                break;
+            }
+
+            case "headers": {
+                for (let headerName of Object.keys(actions.headers)) {
+                    res.set(headerName, actions.headers[headerName]);
+                }
+
+                break;
+            }
+
+            case "body": {
+                res.send(actions.body);
+                break;
+            }
+
+            case "forward-to": {
+                let urlPrefix = actions._urlPrefix || "";
+                await forwardRequest(actions["forward-to"], urlPrefix, req, res);
+                break;
+            }
+
+            case "finish": {
+                finish = true;
+                break;
+            }
+        }
+    }
+
+    return finish;
+}
+
+/**
+ * Weiterleiten der übergebenen HTTP-Anfrage ein einen Backendserver. Im ersten Parameter
+ * wird hierfür eine Liste mit Zieladressen übergeben, im zweiten Parameter der durch die
+ * Regeln erkannte URL-Prefix, damit dieser vor der Weiterleitung abgeschnitten werden kann. 
+ * 
+ * @param {Array[string]} forwardTo Liste mit möglichen Zieladressen
+ * @param {string} urlPrefix Gematchter URL-Prefix, vor Weiterleitung entfernen
+ * @param {Express.Request} req HTTP-Anfrage
+ * @param {Express.Response} res HTTP-Antwort
+ */
+async function forwardRequest(forwardTo, urlPrefix, req, res) {
+    // Ziel-URL ermitteln (mit Load Balancing)
+    let loadBalancerKey   = JSON.stringify(forwardTo);
+    let loadBalancerIndex = loadBalancer[loadBalancerKey] || -1;
+
+    loadBalancerIndex = (loadBalancerIndex + 1) % forwardTo.length;
+    loadBalancer[loadBalancerKey] = loadBalancerIndex;
+
+    let urlSuffix = req.originalUrl.slice(urlPrefix.length);
+    while (urlSuffix.startsWith("/")) urlSuffix = urlSuffix.slice(1);
+
+    let targetUrl = `${forwardTo[loadBalancerIndex]}/${urlSuffix}`;
+    logger.info(`Weiterleitung der Anfrage an: ${targetUrl}`);
+
+    // Anfrage weiterleiten
+    await new Promise((resolve, reject) => {
+        let forwardReq = http.request(targetUrl, {joinDuplicateHeaders: true}, forwardRes => {
+            res.writeHead(forwardRes.statusCode, forwardRes.headersDistinct);
+            forwardRes.pipe(res);
+            resolve();
+        });
+    
+        forwardReq.on("error", error => {
+            reject(error);
+        });
+
+        for (let headerName of Object.keys(req.headersDistinct)) {
+            forwardReq.setHeader(headerName, req.headersDistinct[headerName]);
+        }
+    
+        let forwardedHeaderValue = "";
+    
+        if (req.socket.localAddress) {
+            forwardedHeaderValue += `;by=${req.socket.localAddress}:${req.socket.localPort}`;
+            forwardedHeaderValue += `;for=${req.socket.remoteAddress}:${req.socket.remotePort}`;
+        }
+    
+        if (req.headers.host) forwardedHeaderValue += `;host=${req.headers.host}`;
+        if (req.protocol) forwardedHeaderValue += `;proto=${req.protocol}`;
+        while (forwardedHeaderValue.startsWith(";")) forwardedHeaderValue = forwardedHeaderValue.slice(1);
+    
+        forwardReq.appendHeader("forwarded", forwardedHeaderValue);
+    
+        req.pipe(forwardReq);
+    });
+    
+}
