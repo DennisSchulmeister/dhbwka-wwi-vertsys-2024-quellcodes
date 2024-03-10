808f6ee3c3ed39da6e81fce38a3f45969c39450c
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-03-10 22:09:03 +0100

    MQTT-Demo: Entkopplung der tmux Sessions für Parallelnutzung beider Demos

--- 4 Asynchroner Nachrichtenaustausch/Beispiele/MQTT-Demo/demo-load-balancing.sh
+++ 4 Asynchroner Nachrichtenaustausch/Beispiele/MQTT-Demo/demo-load-balancing.sh
@@ -1,37 +1,41 @@
 #!/bin/sh
 
 export MQTT_TOPIC="vorlesung/beispiel2"
+export TMUX_S="demo-lb"
 
-tmux new-session -d -s "pubsub-demo"
+tmux new-session -d -s $TMUX_S -e MQTT_TOPIC=$MQTT_TOPIC -e TMUX_S=$TMUX_S
+# -t session:window.pane
+# Ctrl-b q to identify the panes
 
-tmux split-window -h
-tmux split-window -v -t "%0"
-tmux split-window -v -t "%0"
-tmux split-window -v -t "%1"
-tmux split-window -v -t "%1"
+tmux split-window -t $TMUX_S -h
+tmux split-window -t $TMUX_S -v
+tmux split-window -t $TMUX_S -v
+tmux select-pane -t $TMUX_S:0.0
+tmux split-window -t $TMUX_S -v
+tmux split-window -t $TMUX_S -v
 
-tmux send-keys -t 0 "./cmd/pub.sh" C-m
-tmux send-keys -t 1 "./cmd/sub-lb1.sh" C-m
-tmux send-keys -t 2 "./cmd/sub-lb1.sh" C-m
-tmux send-keys -t 3 "./cmd/sub-lb2.sh" C-m
-tmux send-keys -t 4 "./cmd/sub-lb2.sh" C-m
-tmux send-keys -t 5 "./cmd/sub-lb2.sh" C-m
+tmux send-keys -t $TMUX_S:0.0 "./cmd/pub.sh" C-m
+tmux send-keys -t $TMUX_S:0.1 "./cmd/sub-lb1.sh" C-m
+tmux send-keys -t $TMUX_S:0.2 "./cmd/sub-lb1.sh" C-m
+tmux send-keys -t $TMUX_S:0.3 "./cmd/sub-lb2.sh" C-m
+tmux send-keys -t $TMUX_S:0.4 "./cmd/sub-lb2.sh" C-m
+tmux send-keys -t $TMUX_S:0.5 "./cmd/sub-lb2.sh" C-m
 
-tmux set -g default-terminal "screen-256color"
-tmux set -as terminal-features ",$TERM*:RGB"
-tmux set -g mouse on
+tmux set -t $TMUX_S -g default-terminal "screen-256color"
+tmux set -t $TMUX_S -as terminal-features ",$TERM*:RGB"
+tmux set -t $TMUX_S -g mouse on
 
-tmux set -g pane-active-border-style bg=#234973,fg=#428ccc
-tmux set -g pane-border-style bg=#234973,fg=#428ccc
-tmux set -g status-style bg=black,fg=#f0f0f0
+tmux set -t $TMUX_S -g pane-active-border-style bg=#234973,fg=#428ccc
+tmux set -t $TMUX_S -g pane-border-style bg=#234973,fg=#428ccc
+tmux set -t $TMUX_S -g status-style bg=black,fg=#f0f0f0
 
-tmux select-pane -t 0 -P bg=#333333,fg=#f0f0f0
-tmux select-pane -t 1 -P bg=#275e3c,fg=#c8dacf
-tmux select-pane -t 2 -P bg=#275e3c,fg=#c8dacf
-tmux select-pane -t 3 -P bg=#234973,fg=#d6dfe9
-tmux select-pane -t 4 -P bg=#234973,fg=#d6def9
-tmux select-pane -t 5 -P bg=#234973,fg=#d6def9
+tmux select-pane -t $TMUX_S:0.0 P bg=#333333,fg=#f0f0f0
+tmux select-pane -t $TMUX_S:0.1 -P bg=#275e3c,fg=#c8dacf
+tmux select-pane -t $TMUX_S:0.2 -P bg=#275e3c,fg=#c8dacf
+tmux select-pane -t $TMUX_S:0.3 -P bg=#234973,fg=#d6dfe9
+tmux select-pane -t $TMUX_S:0.4 -P bg=#234973,fg=#d6def9
+tmux select-pane -t $TMUX_S:0.5 -P bg=#234973,fg=#d6def9
 
-tmux select-pane -t 0
+tmux select-pane -t $TMUX_S:0.0
 
-tmux attach-session -t "pubsub-demo"
+tmux attach-session -t $TMUX_S
