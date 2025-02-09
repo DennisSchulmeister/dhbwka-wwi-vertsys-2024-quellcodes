46fdac786543fe9f63d19ecacb20d355c81885af
Dennis Schulmeister-Zimolong <dennis@patk.org>
2025-02-09 04:59:42 +0100

    Erste Schritte mit Express

--- Beispiele/Erste Schritte mit Express/README.md
+++ Beispiele/Erste Schritte mit Express/README.md
@@ -1,4 +1,13 @@
 Erste Schritte mit Express
 ==========================
 
-TODO: Beispiel wird gerade erstellt ...
+Dieses Beispiel zeigt die ersten Schritte zur Programmierung serverseitiver Webanwendungen
+oder später auch Webservices mit dem Express-Framework. Im Vorlesungsskript wird das Programm
+Schritt für Schritt beschrieben.
+
+1. Anlegen des Projekts
+1. Express 4 vs. Express 5
+1. Konfiguration über Umgebungsvariablen
+1. Express-Server initialisieren und starten
+1. Behandlung von GET-Anfragen mit Query. und Pfad-Parametern
+1. Middleware-Funktionen z.B. zur Protokollierung aller Anfragen
---
+++ Beispiele/Erste Schritte mit Express/src/main.js
@@ -0,0 +1,83 @@
+/**
+ * Unsere allerersten Schritte mit dem Express Framework. Das Beispiel zeigt die
+ * allgemeine Struktur einer Express-Anwendung mit dem `app`-Objekt, Middleware
+ * und Request Handler Funktionen.
+ */
+
+import dotenv  from "dotenv";
+import express from "express";
+import logging from "logging";
+
+// Programmname ausgeben
+console.log("Erste Schritte mit Express");
+console.log("==========================");
+console.log();
+
+// Konfigurationsvariablen einlesen. Streng genommen gehört dies nicht zu Express.
+// Um das Deployment in einer Cloud-Umgebung zu vereinfachen, ist es heute aber
+// für Server-Anwendungen üblich, Konfigurationswerte aus den Umgebungsvariablen
+// des Betriebssystems zu bezeiehen. Vgl. https://www.12factor.net/
+dotenv.config();
+
+const config = {
+    host: process.env.LISTEN_HOST || "",
+    port: process.env.LISTEN_PORT || 9000,
+};
+
+const logger = logging.default("main");
+
+/**
+ * Zentrales App-Objekt der Express-Anwendung. Dies ist der eigentlichen Webserver.
+ * Weiter unten registrieren wir verschiedene Handler Funktionen, die es uns ermöglichen,
+ * die eingehenden HTTP-Anfragen zu beantworten.
+ */
+const app = express();
+
+/**
+ * `app.use()`: Registrieren einer Middleware-Funktion. Middleware-Funktionen werden
+ * für alle HTTP-Anfragen aufgerufen, unabhängig von der HTTP-Methode oder URL, um
+ * die Anfrage vorzuverarbeiten oder die Antwort zu filtern, bevor sie an den Client
+ * geschickt wird.
+ */
+app.use((req, res, next) => {
+    logger.info(`${req.method} ${req.originalUrl}`);
+
+    // Wichtig: Mit next() wird die "nächste" Middleware-Funktion bzw. die tatsächlich
+    // zuständige Handler-Funktion aufgerufen. Nach dieser Zeile finden wir im `res`-Objekt
+    // die von der Handler-Funktion generierten Antwortdaten, so dass wir diese hier nochmal
+    // anpassen könnten.
+    next();
+});
+
+/**
+ * Request Handler Funkion für GET-Anfragen an die URL "/". Man nennt dies auch
+ * den "Endpunkt GET /". Über den Query-Parameter `?name=…` kann ein Name übergeben
+ * werden, der begrüßt werden soll.
+ */
+app.get("/", (req, res) => {
+    res.status(200);
+
+    if (req.query.name) {
+        res.send(`Hallo ${req.query.name}`);
+    } else {
+        res.send(`Hallo! Sage mir deinen Namen durch einen Aufruf von ${req.url}?name=… oder /hello/…`);
+    }
+});
+
+/**
+ * Request Handler mit benanntem URL-Parameter. Der zu grüßende Name wird hier als
+ * Teil des URL-Pfads übergeben, wodurch die URL sehr viel schöner aussieht.
+ * (Echten Webentwicklern ist so etwas wichtig 🙂)
+ */
+app.get("/hello/:name", (req, res) => {
+    res.status(200);
+    res.send(`Hallo ${req.params.name}`);
+});
+
+/**
+ * Webserver starten. Die Callback-Funktion wird aufgefufen, sobald der Webserver
+ * bereit ist, Anfragen zu bearbeiten.
+ */
+app.listen(config.port, config.host, () => {
+    logger.info(`Server lauscht auf ${config.host}:${config.port}`);
+});
